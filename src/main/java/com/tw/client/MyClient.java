aa53b88aac840c39f1678a682ac2ec028136eb5c
FengBo <fungbo@126.com>
2015-07-14 21:59:17 +0800

    Client uses Naming instead of Context

--- src/main/java/com/tw/client/MyClient.java
+++ src/main/java/com/tw/client/MyClient.java
@@ -2,26 +2,18 @@ package com.tw.client;
 
 import com.tw.server.MyService;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 public class MyClient {
     private static final String URL = "rmi://localhost:30005/";
-    private static final Context CONTEXT;
 
-    static {
-        try {
-            CONTEXT = new InitialContext();
-        } catch (NamingException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public static void main(String[] args) throws NamingException, RemoteException {
+    public static void main(String[] args) throws NamingException, RemoteException, MalformedURLException, NotBoundException {
         System.out.println("Client start...");
-        MyService service = (MyService) CONTEXT.lookup(URL + "Service");
+        MyService service = (MyService) Naming.lookup(URL + "Service");
         System.out.println(service.getName());
     }
 }
